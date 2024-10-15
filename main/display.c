b41b1ec697f65e1f59146d09ae87a274fc36a6d5
Peter Wyrd <thewyrdguy@gmail.com>
2024-10-15 22:42:26 +0200

    Some games with display layout

--- main/display.c
+++ main/display.c
@@ -29,7 +29,50 @@ static inline lv_color_t c_swap(lv_color_t o)
 }
 #endif
 
-lv_obj_t *welcome_label, *update_label;
+/*
+ * In case it is needed:
+ *  https://forum.lvgl.io/t/get-string-width-in-pixels/3414/4
+ * lv_point_t p;
+ * _lv_txt_ap_proc(my_str, ctx_text);
+ * _lv_txt_get_size(&p, ctxi_text, my_font, 0, 0,
+ * 			 LV_COORD_MAX, LV_TXT_FLAG_EXPAND);
+ */
+
+lv_obj_t *welcome_label, *update_label, *batt_label, *hr_label;
+
+lv_obj_t *mkframe(lv_obj_t *parent, lv_align_t align,
+		int32_t w, int32_t h, lv_color_t bcolour)
+{
+	lv_obj_t *frame = lv_obj_create(parent);
+	lv_obj_set_align(frame, align);
+	lv_obj_set_size(frame, w, h);
+	lv_obj_set_style_bg_opa(frame, LV_OPA_0, LV_PART_MAIN);
+	lv_obj_set_style_border_color(frame, bcolour, LV_PART_MAIN);
+	lv_obj_set_style_border_opa(frame, LV_OPA_100, LV_PART_MAIN);
+	lv_obj_set_style_border_width(frame, 2, LV_PART_MAIN);
+	lv_obj_set_style_radius(frame, 5, LV_PART_MAIN);
+	lv_obj_set_style_pad_all(frame, 5, LV_PART_MAIN);
+	return frame;
+}
+
+lv_obj_t *mklabel(lv_obj_t *parent, lv_obj_t *after)
+{
+	lv_obj_t *label = lv_label_create(parent);
+	lv_obj_set_size(label, lv_pct(100), 30);
+	lv_obj_set_style_text_font(label, &lv_font_montserrat_28, 0);
+	lv_obj_set_style_text_color(label,
+			c_swap(lv_color_hex(0xffffff)), LV_PART_MAIN);
+	lv_obj_set_style_bg_color(label, c_swap(lv_color_hex(0x007700)),
+				LV_PART_MAIN);
+	lv_obj_set_style_bg_opa(label, LV_OPA_100, LV_PART_MAIN);
+	lv_obj_set_style_text_align(label, LV_ALIGN_CENTER, LV_PART_MAIN);
+	if (after) {
+		lv_obj_align_to(label, after, LV_ALIGN_OUT_BOTTOM_MID, 0, 3);
+	} else {
+		lv_obj_align(label, LV_ALIGN_TOP_MID, 0, 0);
+	}
+	return label;
+}
 
 void display_welcome(lv_display_t* disp)
 {
@@ -37,20 +80,12 @@ void display_welcome(lv_display_t* disp)
 	lv_obj_set_style_bg_color(scr, c_swap(lv_color_hex(0x000000)),
 				LV_PART_MAIN);
 
-	lv_obj_t *frame = lv_obj_create(scr);
-	lv_obj_set_align(frame, LV_ALIGN_LEFT_MID);
-	lv_obj_set_size(frame, 460, 240);
-	lv_obj_set_style_bg_color(frame, c_swap(lv_color_hex(0x070707)),
-				LV_PART_MAIN);
-	lv_obj_set_style_bg_opa(frame, LV_OPA_0, LV_PART_MAIN);
-	lv_obj_set_style_border_color(frame, c_swap(lv_color_hex(0x770000)),
-			LV_PART_MAIN);
-	lv_obj_set_style_border_opa(frame, LV_OPA_100, LV_PART_MAIN);
-	lv_obj_set_style_border_width(frame, 2, LV_PART_MAIN);
-	lv_obj_set_style_radius(frame, 5, LV_PART_MAIN);
-	// lv_obj_add_event_cb(frame, frame_event_cb, LV_EVENT_DRAW_MAIN, NULL);
+	lv_obj_t *lframe = mkframe(scr, LV_ALIGN_LEFT_MID, 460, lv_pct(100),
+			c_swap(lv_color_hex(0x770000)));
+	lv_obj_t *rframe = mkframe(scr, LV_ALIGN_RIGHT_MID, 70, lv_pct(100),
+			c_swap(lv_color_hex(0x007700)));
 
-	welcome_label = lv_label_create(scr);
+	welcome_label = lv_label_create(lframe);
 	lv_obj_set_style_bg_color(welcome_label, c_swap(lv_color_hex(0x000077)),
 				LV_PART_MAIN);
 	lv_obj_set_style_bg_opa(welcome_label, LV_OPA_100, LV_PART_MAIN);
@@ -64,11 +99,11 @@ void display_welcome(lv_display_t* disp)
 			c_swap(lv_color_hex(0xffffff)), LV_PART_MAIN);
 	lv_obj_align(welcome_label, LV_ALIGN_CENTER, 0, 0);
 
-	update_label = lv_label_create(scr);
+	update_label = lv_label_create(lframe);
 	lv_obj_set_style_text_font(update_label, &lv_font_montserrat_28, 0);
 	lv_obj_set_style_text_color(update_label,
 			c_swap(lv_color_hex(0xffffff)), LV_PART_MAIN);
-	lv_obj_set_width(update_label, lv_pct(45));
+	lv_obj_set_width(update_label, lv_pct(75));
 	lv_obj_set_height(update_label, lv_pct(15));
 	lv_obj_set_style_bg_color(update_label, c_swap(lv_color_hex(0x007700)),
 				LV_PART_MAIN);
@@ -76,6 +111,23 @@ void display_welcome(lv_display_t* disp)
 	lv_obj_set_style_text_align(update_label, LV_ALIGN_LEFT_MID,
 				LV_PART_MAIN);
 	lv_obj_align(update_label, LV_ALIGN_TOP_RIGHT, 0, 0);
+
+	batt_label = mklabel(rframe, NULL);
+	hr_label = mklabel(rframe, batt_label);
+	lv_obj_t *label_2 = mklabel(rframe, hr_label);
+	lv_obj_t *label_3 = mklabel(rframe, label_2);
+	lv_obj_t *label_4 = mklabel(rframe, label_3);
+	lv_obj_t *label_5 = mklabel(rframe, label_4);
+	lv_obj_t *label_6 = mklabel(rframe, label_5);
+
+	lv_label_set_text_static(batt_label, "55%");
+	lv_label_set_text_static(hr_label, "255");
+	lv_label_set_text_static(label_2, "2");
+	lv_label_set_text_static(label_3, "3");
+	lv_label_set_text_static(label_4, "4");
+	lv_label_set_text_static(label_5, "5");
+	lv_label_set_text_static(label_6, "6");
+
 }
 
 void display_update(int samples)
